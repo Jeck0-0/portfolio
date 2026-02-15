44b16313fedf39df82bf2f60c8110196d84560e8
Giacomo <jrubboli@gmail.com>
2026-02-15 19:42:26 +0400

    chore: setup standalone wasm and github actions deployment

---
+++ Portfolio.Client/Program.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.AspNetCore.Components.Web;
+using Portfolio.Client.Components;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<Routes>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
